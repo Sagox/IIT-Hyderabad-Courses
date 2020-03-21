2ecad04a307d2dbb6abe6482a450f8cec4c81812
Sagar Jain <j.sagar700@gmail.com>
2020-03-21 23:32:31 +0530

    Reduced number of edges

--- utilities/adjacencyListGen.py
+++ utilities/adjacencyListGen.py
@@ -1,14 +1,14 @@
 import random
 import time
 import sys
-
+import math
 random.seed(time.time())
 numberOfNodes = int(sys.argv[1])
 population = range(1, numberOfNodes+1)
 for i in range(1, numberOfNodes+1):
 	print(i, end=' ')
 	# ranodom number of neighbours
-	k = random.randrange(1, numberOfNodes+1)
+	k = random.randrange(1,math.ceil(numberOfNodes/4.0))
 	# random k nodes among all the vertices
 	for j in random.sample(population, k):
 		if j != i:
